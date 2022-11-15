03056f5b469b19302392c5c152faf198b0336c69
Elvia Arteaga <70364657+Elvi98@users.noreply.github.com>
2022-11-15 15:32:51 -0500

    Add History
    Component & View

---
+++ src/History/pages/HistoryView.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="3">
+        <Profile />
+      </v-col>
+
+      <v-col cols="9">
+        <History />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import History from '../components/History'
+import Profile from "@/User/components/Profile";
+
+export default {
+  name: 'Simulator',
+
+  components: {
+    History,
+    Profile
+  },
+}
+</script>

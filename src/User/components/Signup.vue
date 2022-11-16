15bed17c9064b5581a842745a42786080df96633
Elvia Arteaga <70364657+Elvi98@users.noreply.github.com>
2022-11-15 20:13:15 -0500

    Add validation to sign up form

--- src/User/components/Signup.vue
+++ src/User/components/Signup.vue
@@ -5,104 +5,190 @@
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md7>
             <v-card class="elevation-12 pa-10 rounded-xl">
-              <v-card-text>
-                <v-card-title class="font-weight-bold">
-                  Te damos la bienvenida a Alpaca Finance
-                </v-card-title>
-                <v-form>
+              <v-card-title class="font-weight-bold">
+                Te damos la bienvenida a Alpaca Finance
+              </v-card-title>
+              <validation-observer
+                  ref="observer"
+                  v-slot="{ invalid }"
+              >
+                <form @submit.prevent="submit">
                   <v-row>
                     <v-col>
-                      <v-text-field
-                          v-model="firstName"
-                          :rules="[rules.required, rules.firstNameRules]"
+                      <validation-provider
+                          v-slot="{ errors }"
                           name="firstName"
-                          label="Nombres"
-                          type="text"
-                          :counter="15"
-                      ></v-text-field>
+                          rules="required|max:15"
+                      >
+                        <v-text-field
+                            v-model="firstName"
+                            :counter="15"
+                            :error-messages="errors"
+                            label="Nombres"
+                            required
+                        ></v-text-field>
+                      </validation-provider>
                     </v-col>
                     <v-col>
-                      <v-text-field
-                          v-model="lastName"
-                          :rules="[rules.required, rules.lastNameRules]"
+                      <validation-provider
+                          v-slot="{ errors }"
                           name="lastName"
-                          label="Apellidos"
-                          type="text"
-                          :counter="35"
-                      ></v-text-field>
+                          rules="required|max:30"
+                      >
+                        <v-text-field
+                            v-model="lastName"
+                            :counter="30"
+                            :error-messages="errors"
+                            label="Apellidos"
+                            required
+                        ></v-text-field>
+                      </validation-provider>
                     </v-col>
                   </v-row>
-                  <v-text-field
-                      v-model="email"
-                      :rules="[rules.required, rules.email]"
+                  <validation-provider
+                      v-slot="{ errors }"
                       name="email"
-                      label="Email"
-                      type="text"
-                  ></v-text-field>
-                  <v-text-field
-                      v-model="password"
-                      :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
-                      :rules="[rules.required, rules.min]"
-                      :type="showPass ? 'text' : 'password'"
-                      name="password"
-                      label="Contraseña"
-                      hint="At least 8 characters"
-                      @click:append="showPass = !showPass"
-                  ></v-text-field>
-                  <p class="subtitle-1 font-weight-bold" to="/historial" link>
+                      rules="required|email"
+                  >
+                    <v-text-field
+                        v-model="email"
+                        :error-messages="errors"
+                        label="E-mail"
+                        required
+                    ></v-text-field>
+                  </validation-provider>
+
+                  <!--TO-DO:
+                  Password-->
+
+                  <p class="subtitle-1 font-weight-bold">
                     Información de la Cuenta
                   </p>
+
                   <v-row>
                     <v-col cols="4">
-                      <v-select
-                          :items="documentType"
-                          label="Tipo de documento"
-                          solo
-                      ></v-select>
+                      <validation-provider
+                          v-slot="{ errors }"
+                          name="document type"
+                          rules="required"
+                      >
+                        <v-select
+                            v-model="documentType"
+                            :items="documentTypeList"
+                            :error-messages="errors"
+                            label="Tipo de documento"
+                            data-vv-name="documentTypeSelect"
+                            required
+                            solo
+                        ></v-select>
+                      </validation-provider>
                     </v-col>
                     <v-col cols="8">
-                      <v-text-field
-                          v-model="nroDocument"
-                          :rules="[rules.required, rules.nroDocument]"
-                          name="nroDocument"
-                          label="Número de documento"
-                          type="number"
-                      ></v-text-field>
+                      <validation-provider
+                          v-slot="{ errors }"
+                          name="document number"
+                          :rules="{
+                        required: true,
+                        digits: 8,
+                  }"
+                      >
+                        <v-text-field
+                            v-model="documentNumber"
+                            :counter="8"
+                            :error-messages="errors"
+                            label="Número de documento"
+                            required
+                        ></v-text-field>
+                      </validation-provider>
                     </v-col>
                   </v-row>
+
                   <v-row>
                     <v-col cols="4">
-                      <v-select
-                          :items="countryCode"
-                          label="Código de País"
-                          solo
-                      ></v-select>
+                      <validation-provider
+                          v-slot="{ errors }"
+                          name="country code"
+                          rules="required"
+                      >
+                        <v-select
+                            v-model="countryCode"
+                            :items="countryCodeList"
+                            :error-messages="errors"
+                            label="Código de País"
+                            data-vv-name="countryCodeSelect"
+                            required
+                            solo
+                        ></v-select>
+                      </validation-provider>
                     </v-col>
                     <v-col cols="8">
-                      <v-text-field
-                          v-model="phone"
-                          :rules="[rules.required, rules.phone]"
-                          name="phone"
-                          label="Número de contacto"
-                          type="number"
-                      ></v-text-field>
+                      <validation-provider
+                          v-slot="{ errors }"
+                          name="phoneNumber"
+                          :rules="{
+                        required: true,
+                        digits: 9,
+                        regex: '^(9)\\d{8}$'
+                  }"
+                      >
+                        <v-text-field
+                            v-model="phoneNumber"
+                            :counter="9"
+                            :error-messages="errors"
+                            label="Phone Number"
+                            required
+                        ></v-text-field>
+                      </validation-provider>
                     </v-col>
                   </v-row>
-                  <v-card-actions>
-                    <v-row
-                        class="pa-5"
-                        align="center"
-                        justify="space-around">
-                      <v-btn
-                          width="80%"
-                          rounded
-                          color="primary"
-                          to="/">Finalizar
-                      </v-btn>
-                    </v-row>
-                  </v-card-actions>
-                </v-form>
-              </v-card-text>
+
+                  <validation-provider
+                      v-slot="{ errors }"
+                      rules="mustAccept"
+                      name="age checkbox"
+                  >
+                    <v-checkbox
+                        v-model="ageCheckbox"
+                        :error-messages="errors"
+                        value="1"
+                        label="Declaro tener +18 años de edad."
+                        type="checkbox"
+                        required
+                    ></v-checkbox>
+                  </validation-provider>
+
+                  <validation-provider
+                      v-slot="{ errors }"
+                      rules="mustAccept"
+                      name="terms agree checkbox"
+                  >
+                    <v-checkbox
+                        v-model="termsAgreeCheckbox"
+                        :error-messages="errors"
+                        value="1"
+                        label="Acepto los términos y condiciones de Alpaca Finance."
+                        type="checkbox"
+                        required
+                    ></v-checkbox>
+                  </validation-provider>
+
+                  <v-row
+                      class="pa-5"
+                      align="center"
+                      justify="space-around">
+                    <v-btn
+                        width="80%"
+                        rounded
+                        color="primary"
+                        to="/"
+                        type="submit"
+                        :disabled="invalid"
+                    >
+                      Finalizar
+                    </v-btn>
+                  </v-row>
+                </form>
+              </validation-observer>
             </v-card>
           </v-flex>
         </v-layout>
@@ -112,36 +198,64 @@
 </template>
 
 <script>
+import {required, digits, email, max, regex} from 'vee-validate/dist/rules'
+import {extend, ValidationObserver, ValidationProvider, setInteractionMode} from 'vee-validate'
+
+setInteractionMode('eager')
+
+extend('digits', {
+  ...digits,
+  message: '{_field_} needs to be {length} digits. ({_value_})',
+})
+
+extend('required', {
+  ...required,
+  message: '{_field_} can not be empty',
+})
+
+extend('mustAccept', {
+  ...required,
+  message: 'Required',
+})
+
+extend('max', {
+  ...max,
+  message: '{_field_} may not be greater than {length} characters',
+})
+
+extend('regex', {
+  ...regex,
+  message: '{_field_} {_value_} does not match {regex}',
+})
+
+extend('email', {
+  ...email,
+  message: 'Email must be valid',
+})
+
 export default {
-  name: 'Login',
-  props: {
-    source: String,
+  components: {
+    ValidationProvider,
+    ValidationObserver,
   },
-  data() {
-    return {
-      email: '',
-      showPass: false,
-      password: '',
-      firstName: '',
-      lastName: '',
-      documentType: ['DNI', 'Carnet de Extranjería', 'Pasaporte'],
-      nroDocument: '',
-      countryCode: ['+51 (Perú)', '+52 (México)', '+56 (Chile)', '+57 (Colombia)'],
-      phone:'',
-      rules: {
-        required: value => !!value || 'Required.',
-        min: v => v.length >= 8 || 'Min 8 characters',
-        emailMatch: () => (`The email and password you entered don't match`),
-        email: value => {
-          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          return pattern.test(value) || 'Invalid e-mail.'
-        },
-        firstNameRules: v => v.length <= 15 || 'Firstname must be less than 15 characters',
-        lastNameRules: v => v.length <= 35 || 'Lastname must be less than 35 characters',
-        nroDocument: v => v.length == 8 || 'Document number must be 8 characters',
-        phone: v => v.length >= 9 || 'Min 8 characters',
-      },
-    }
+  data: () => ({
+    firstName: '',
+    lastName: '',
+    email: '',
+    documentType: '',
+    documentTypeList: ['DNI', 'Carnet de Extranjería', 'Pasaporte'],
+    documentNumber:'',
+    countryCode:'',
+    countryCodeList:['+51 (Perú)', '+52 (México)', '+56 (Chile)', '+57 (Colombia)'],
+    phoneNumber: '',
+    ageCheckbox: null,
+    termsAgreeCheckbox: null,
+  }),
+
+  methods: {
+    submit() {
+      this.$refs.observer.validate()
+    },
   },
-};
+}
 </script>
